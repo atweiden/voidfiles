795259bd9262301508772a05184e2da6ef183bb8
Andy Weidenbaum <archbaum@gmail.com>
2019-08-28 06:22:45 -0400

    reorder groups alphabetically

--- .vim/pack/install.sh
+++ .vim/pack/install.sh
@@ -64,12 +64,12 @@ package() {
 (set_dir 'comments' 'start'
  package 'tpope/vim-commentary' &
  wait) &
+(set_dir 'filesystem' 'start'
+ package 'duggiefresh/vim-easydir' &
+ wait) &
 (set_dir 'folds' 'start'
  package 'Konfekt/FastFold' &
  package 'Harenome/vim-neatfoldtext' &
- wait) &
-(set_dir 'filesystem' 'start'
- package 'duggiefresh/vim-easydir' &
  wait) &
 (set_dir 'keyboard' 'opt'
  package 'kana/vim-arpeggio' &
